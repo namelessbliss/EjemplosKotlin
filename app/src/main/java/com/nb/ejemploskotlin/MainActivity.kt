4536a9fe65d8562113a639b35222dcd3121aa3f5
namelessbliss <anonimo2911.2012@gmail.com>
2019-10-30 22:24:37 -0500

    Limpiando

--- app/src/main/java/com/nb/ejemploskotlin/MainActivity.kt
+++ app/src/main/java/com/nb/ejemploskotlin/MainActivity.kt
@@ -1,22 +1,12 @@
 package com.nb.ejemploskotlin
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
-    var contador = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        //obtener viewcomponent con el mismo nombre de su id
-        btnContador.setOnClickListener { view ->
-            contador++
-            Toast.makeText(this, "Clic en contador: $contador", Toast.LENGTH_SHORT).show()
-        }
     }
 }
