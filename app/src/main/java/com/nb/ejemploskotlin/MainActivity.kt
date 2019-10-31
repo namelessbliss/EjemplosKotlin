6a0d2d3fb3383ebbbf6413ce60cedbe6c88706a1
namelessbliss <anonimo2911.2012@gmail.com>
2019-10-30 22:17:28 -0500

    Variables en kotlin

--- app/src/main/java/com/nb/ejemploskotlin/MainActivity.kt
+++ app/src/main/java/com/nb/ejemploskotlin/MainActivity.kt
@@ -1,9 +1,20 @@
 package com.nb.ejemploskotlin
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    //Variables cuyo valor no puede ser modificado
+    val PI: Float = 3.14f
+
+    //Variable que permite modificar valor
+    var contador: Int = 0;
+
+    var saludo = "Hello world"
+
+    //variable nula
+    var saludo2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
