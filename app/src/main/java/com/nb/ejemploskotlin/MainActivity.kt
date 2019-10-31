bcb3061fd1eebcded30ae840e7e76cd98468eb39
namelessbliss <anonimo2911.2012@gmail.com>
2019-10-30 22:18:02 -0500

    Limpiando

--- app/src/main/java/com/nb/ejemploskotlin/MainActivity.kt
+++ app/src/main/java/com/nb/ejemploskotlin/MainActivity.kt
@@ -5,17 +5,6 @@ import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-    //Variables cuyo valor no puede ser modificado
-    val PI: Float = 3.14f
-
-    //Variable que permite modificar valor
-    var contador: Int = 0;
-
-    var saludo = "Hello world"
-
-    //variable nula
-    var saludo2: String? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
